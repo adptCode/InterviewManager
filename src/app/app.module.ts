0f20c75888e38b77a7cb927739dd55e6b8417cb1
Alessandro De Pietri Tonelli <adepietri.it@gmail.com>
2024-12-10 12:16:43 +0100

    remove filter comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FormComponent } from './components/form/form.component';
 import { ListComponent } from './components/list/list.component';
-import { FilterComponent } from './components/filter/filter.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
-    ListComponent,
-    FilterComponent
+    ListComponent
   ],
   imports: [
     BrowserModule,
